b6c06b010c318baa25ee856d42d34d2393d9ca16
Алина Поляцкая <55145202+alina-poliatska@users.noreply.github.com>
2020-12-01 19:59:45 +0200

    Add files via upload

---
+++ Lab3.py
@@ -0,0 +1,20 @@
+from PIL import Image, ImageDraw
+import math
+
+img = Image.new('RGBA', (960, 960), (255, 255, 255))
+draw = ImageDraw.Draw(img)
+angle = math.radians(50)
+
+
+with open("DS4.txt", "r") as file:
+    for line in file:
+        coords_list = line.split()
+        i = int(coords_list[0]) - 480
+        j = int(coords_list[1]) - 480
+        x = math.cos(angle) * i - math.sin(angle) * j
+        y = math.sin(angle) * i + math.cos(angle) * j
+        draw.line((x + 480, y + 480, x + 481, y + 481), fill=(0, 190, 255))
+
+img.show()
+img.save('res.png')
+
